bad59ad486002d77900f27f99542ae249628bff0
lucas <lucasmunafo88@gmail.com>
2019-12-03 02:31:44 -0300

    Agregando metodos del tablero y del main.

---
+++ main.py
@@ -0,0 +1,25 @@
+from tablero import tablero
+
+class tic_tac_toe(object):
+    def comenzar_partida(self):
+        partida = tablero()
+
+        a.insertar_posicion()
+        a.verificar_tablero()
+        a.insertar_posicion()
+        a.verificar_tablero()
+        a.insertar_posicion()
+        a.verificar_tablero()
+
+        movimientos = range(9)
+        for turno in movimientos:
+                        
+
+    def victoria(self, player):
+        print("El jugador {} ha ganado!!").format(player)
+
+
+if __name__ == '__main__':
+    juego = tic_tac_toe()
+    juego.comenzar_partida()
+
---
+++ tablero.py
@@ -0,0 +1,74 @@
+from os import system
+import numpy as np
+
+class tablero(object):
+
+    POSICIONES_VALIDAS = ["A1","A2","A3","B1","B2","B3","C1","C2","C3"]
+    posiciones_tablero = np.array((4,4))
+
+    def __init__(self):
+        """
+        Constructor del tablero. Se genera el tablero con las posiciones vacias ('-'). Al mismo tiempo que se genera el tablero se lo dibuja.
+        """
+        self.posiciones_tablero = np.array([
+            [" ","A","B","C"],
+            ["1","-","-","-"],
+            ["2","-","-","-"],
+            ["3","-","-","-"]
+        ])
+        self.dibujar_tablero()
+
+    def dibujar_tablero(self):
+        """
+        Funcion que dibuja el tablero. A partir de la lista de posiciones el mismo redibuja el tablero.
+        """
+        _ = system('clear')
+        for fila in self.posiciones_tablero:
+            for columna in fila:
+                print("| " + columna + " |", end="")
+
+            # ME PARECIO INTERESATE UTILIZAR LO QUE ALEJANDRO ME HABIA COMENTADO EN LA ENTREVISTA, SIN EMBARGO 
+            # CREO QUE SERIA MEJOR REALIZAR UN PRINT LUEGO DE LA SEGUNDA ITERACION
+            # else:
+            #     print("")
+
+            print("")
+        return
+
+    def insertar_posicion(self, simbolo):
+        """
+        Inserta en una posicion valida no ocupada, el simbolo correspondiente (pasado como parametro, permitiendo de esta forma jugar un PVP).
+        """
+        fila = 0
+        columna = 0
+        posicion = input("Ingresa la posicion donde queres jugar...").upper()
+        fila = int(posicion[1])
+        columna = (self.POSICIONES_VALIDAS.index(posicion) // 3) + 1
+        while posicion not in self.POSICIONES_VALIDAS or self.posiciones_tablero[fila][columna] != "-":
+            self.dibujar_tablero()
+            posicion = input("Posicion inexistente o ya utilizada, porfavor reingresa la posicion donde queres jugar...").upper()
+            fila = int(posicion[1])
+            columna = (self.POSICIONES_VALIDAS.index(posicion) // 3) + 1
+
+        self.posiciones_tablero[fila][columna] = simbolo
+        self.dibujar_tablero()
+        return
+
+    def verificar_tablero(self):
+        """
+        Verifica si alguno de los dos jugadores se llevo la victoria.
+        """
+        diagonal_principal = set(np.diag(self.posiciones_tablero))
+        diagonal_secundaria = set(np.diag(np.fliplr(self.posiciones_tablero)))
+        if diagonal_principal == {'X'} or diagonal_secundaria == {'X'}:
+            return 'X'
+        elif diagonal_principal == {'O'} or diagonal_secundaria == {'O'}:
+            return 'O'
+        for i in range(1,4):
+            vertical = set(self.posiciones_tablero[i][1:])
+            horizontal = set(self.posiciones_tablero[1:,i])
+            if vertical == {'X'} or horizontal == {'X'}:
+                return 'X'
+            elif vertical == {'O'} or horizontal == {'O'}:
+                return 'O'
+        return None
